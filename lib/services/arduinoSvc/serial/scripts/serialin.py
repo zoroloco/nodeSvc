8f83c7ecf421a712e2f498e0b0bfee548d028de8
Kenneth Centurion <kcenturion@yahoo.com>
2016-01-21 16:16:47 -0600

    arduino python

--- lib/services/arduinoSvc/serial/scripts/serialin.py
+++ lib/services/arduinoSvc/serial/scripts/serialin.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python2
 
-import threading
-import serial
 
-class serialIn (threading.Thread):
-    def __init__(self, threadID, name,arduino):
-	    threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.name     = name
-		self.arduino  = arduino
-	def run(self):
-	    print "Starting "+ self.name
-		receiveData(self.arduino)
-		arduino.close()
-		
-def receiveData(arduino):
-    lastInput = ''    
-    while 1:
-        input = arduino.readline()#read 1 byte at a time
-        if(lastInput != input):#only stdout data if new
-            sys.stdout.write(input)
-            lastInput = input    
+ 
     
